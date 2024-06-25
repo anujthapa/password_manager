60b6519c2747fde394a2ee75172a1d0223314692
anujthapa <anujthapa@outlook.com>
2024-06-25 23:24:05 +0300

    use cryptography to encrypt and decrypt password

---
+++ password_manager.py
@@ -0,0 +1,39 @@
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb")  as key_file:
+        key_file.write(key)
+
+def load_key():
+    with open("key.key","rb") as file:
+        key  = file.read()
+        return key
+
+key = load_key()
+fer = Fernet(key)
+
+def view():
+    with open("password.txt","r") as f:
+        for line in f.readlines():
+           data = line.rstrip()
+           uname, password = data.split("|")
+           print(f"uname: {uname}, password : {fer.decrypt(password.encode()).decode()}")
+
+def add():
+    name = input("Account name: ")
+    password= input("password: ")
+    with open('password.txt','a') as f:
+        f.write(name + "|" + fer.encrypt(password.encode()).decode() + "\n")
+
+while True:
+    mode =  input("Would youy like to add a new password or view exixting password? (view/add), press Q to quite ").lower()
+    if mode == "q":
+        break
+    if mode == "view" :
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode")
+        continue
